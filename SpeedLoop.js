fdb58855d5abbf572dde44470ee6f242d807bb82
vivekbharatha <vivek.bharatha@gmail.com>
2018-02-28 12:12:24 +0530

    eslint google standard added

--- SpeedLoop.js
+++ SpeedLoop.js
@@ -1,8 +1,8 @@
 const cycles = 1000000000;
 let start = Date.now();
-for (let i=0; i<cycles; i++) {
-
+for (let i = 0; i<cycles; i++) {
+//
 }
 
 let duration = (Date.now() - start)/1000;
-console.log(`looped %d times in %d seconds`,  cycles, duration);
+console.log(`looped %d times in %d seconds`, cycles, duration);
