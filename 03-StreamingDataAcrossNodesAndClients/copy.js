3ed62a062b1573a0816eadfdf01aef094fb3a51d
vivekbharatha <vivek.bharatha@gmail.com>
2018-04-04 12:19:13 +0530

    copy file async

--- 03-StreamingDataAcrossNodesAndClients/copy.js
+++ 03-StreamingDataAcrossNodesAndClients/copy.js
@@ -1,7 +1,20 @@
-// Blunt Attempt
+// Blunt Attempt - Sync
 let fs = require('fs');
+/*
 console.log('copying..');
 var fileBuffer = fs.readFileSync('dummy.txt');
 console.log('Size:', fileBuffer.length);
 fs.writeFileSync('dummyCopied.js', fileBuffer);
 console.log('copying done!');
+*/
+
+// Async
+console.log('copying..');
+fs.readFile('dummy.txt', (readErr, fileBuffer) => {
+	if (readErr) throw readErr;
+	console.log('Size:', fileBuffer.length);
+	fs.writeFile('dummyCopied.txt', fileBuffer, (writeErr) => {
+		if (writeErr) throw writeErr;
+		console.log('copying done!');
+	});
+});
